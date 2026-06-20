c314a3f4fd838d8b11e13b74371aeb517d35eba8
CarolinaAriza-mind <carolinaarizat93@gmail.com>
2026-06-20 10:48:04 -0300

    Creacion de page Sales

--- canarias-frontend/src/types/sale.type.ts
+++ canarias-frontend/src/types/sale.type.ts
@@ -1,4 +1,20 @@
 export interface Sale {
-  sale_id: string;
-  status: string;
+  saleId: string;
+
+  clientId: string;
+
+  totalAmount: number;
+
+  installmentsCount: number;
+
+  saleDate: string;
+
+  status:
+    | "PENDING_ADMIN_VALIDATION"
+    | "PENDING_ENVIRONMENTAL_VISIT"
+    | "PENDING_DELIVERY"
+    | "DELIVERED"
+    | "CLOSED";
+
+  createdAt: string;
 }
